d771321ca26da50dcbdbe05e992c35f2eed82596
Chinelo <chineloezenwelu@gmail.com>
2022-12-24 01:01:35 -0800

    17. Redux pt 2

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,8 @@ import {
   loadProvider, 
   loadNetwork, 
   loadAccount,
-  loadToken 
+  loadTokens, 
+  loadExchange
 } from "../store/interactions"
 
 
@@ -15,16 +16,24 @@ function App() {
   const dispatch = useDispatch()
 
   const loadBlockchainData = async () => {
-    await loadAccount(dispatch)
-    
-
     //connect ethers to blockchain
     const provider = loadProvider(dispatch)
-    const chainId = await loadNetwork(provider, dispatch)
-    
 
-    //connect to token smart contract
-    await loadToken(provider, config[chainId].DApp.address, dispatch)
+    //Fetch current network's chainId(eg hardhat: 31337, kovan: 42)
+    const chainId = await loadNetwork(provider, dispatch)
+
+    //Fetch current account and balance from Metamask
+    await loadAccount(provider, dispatch)
+    
+    //load token smart contracts
+    const DApp = config[chainId].DApp
+    const mETH = config[chainId].mETH
+    await loadTokens(provider, [DApp.address, mETH.address], dispatch)
+
+    //load exchange smart contract
+    const exchangeConfig = config[chainId].exchange
+    await loadExchange(provider, exchangeConfig.address, dispatch)
+
   }
 
 
