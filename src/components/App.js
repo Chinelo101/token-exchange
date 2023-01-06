9c3fa3f4be0a76cf60f9f681f1e3347b14ba116d
Chinelo <chineloezenwelu@gmail.com>
2023-01-06 06:54:08 -0800

    27. My Orders

--- src/components/App.js
+++ src/components/App.js
@@ -1,95 +1,90 @@
-import { useEffect } from "react"
-import { useDispatch } from "react-redux"
-import config from "../config.json"
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import config from '../config.json';
 
-import { 
-  loadProvider, 
-  loadNetwork, 
+import {
+  loadProvider,
+  loadNetwork,
   loadAccount,
-  loadTokens, 
+  loadTokens,
   loadExchange,
   loadAllOrders,
   subscribeToEvents
-} from "../store/interactions"
+} from '../store/interactions';
 
-import Navbar from "./Navbar"
-import Markets from "./Markets"
-import Balance from "./Balance"
-import Order from "./Order"
-import PriceChart from "./PriceChart"
-import Trades from "./Trades"
-import OrderBook from "./OrderBook"
+import Navbar from './Navbar'
+import Markets from './Markets'
+import Balance from './Balance'
+import Order from './Order'
+import PriceChart from './PriceChart'
+import Transactions from './Transactions'
+import Trades from './Trades'
+import OrderBook from './OrderBook'
 
 function App() {
-
   const dispatch = useDispatch()
 
   const loadBlockchainData = async () => {
-    //connect ethers to blockchain
+    // Connect Ethers to blockchain
     const provider = loadProvider(dispatch)
 
-    //Fetch current network's chainId(eg hardhat: 31337, kovan: 42)
+    // Fetch current network's chainId (e.g. hardhat: 31337, kovan: 42)
     const chainId = await loadNetwork(provider, dispatch)
 
-    //Reload page when network (Goerli, HH, etc) changes
-    window.ethereum.on("chainChanged", () => {
-      window.location.reload() //reloads webpage
+    // Reload page when network changes
+    window.ethereum.on('chainChanged', () => {
+      window.location.reload()
     })
 
-    //Fetch current account and balance from Metamask when account is changed
-    window.ethereum.on("accountsChanged", () => {
+    // Fetch current account & balance from Metamask when changed
+    window.ethereum.on('accountsChanged', () => {
       loadAccount(provider, dispatch)
     })
 
-    
-    //load token smart contracts
+    // Load token smart contracts
     const DApp = config[chainId].DApp
     const mETH = config[chainId].mETH
     await loadTokens(provider, [DApp.address, mETH.address], dispatch)
- 
 
-    //load exchange smart contract
+    // Load exchange smart contract
     const exchangeConfig = config[chainId].exchange
     const exchange = await loadExchange(provider, exchangeConfig.address, dispatch)
 
-    //Fetch all orders: open, filled, cancelled
+    // Fetch all orders: open, filled, cancelled
     loadAllOrders(provider, exchange, dispatch)
 
-    //Listen to events 
+    // Listen to events
     subscribeToEvents(exchange, dispatch)
-
   }
 
-
   useEffect(() => {
-    loadBlockchainData() //this will make function show up on website
+    loadBlockchainData()
   })
-
-  //code in the return func gets shown on webpage/app
 
   return (
     <div>
-      <Navbar/>
+
+      <Navbar />
 
       <main className='exchange grid'>
         <section className='exchange__section--left grid'>
 
-          <Markets/>
+          <Markets />
 
-          <Balance/>
+          <Balance />
 
-          <Order/>
+          <Order />
 
         </section>
         <section className='exchange__section--right grid'>
 
-          <PriceChart/>
+          <PriceChart />
 
-          {/* Transactions */}
+          <Transactions />
 
-          <Trades/>
+          <Trades />
 
-          <OrderBook/>
+          <OrderBook />
 
         </section>
       </main>
